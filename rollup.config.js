83e97bb35a56d9f179bb118a33b27c90c103483b
anthonykoch <goaway@github.com>
2020-01-07 16:54:12 -0700

    Update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,34 @@
-import commonjs from 'rollup-plugin-commonjs' // Convert CommonJS modules to ES6
+import commonjs from '@rollup/plugin-commonjs' // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue' // Handle .vue SFC files
-import buble from 'rollup-plugin-buble' // Transpile/polyfill with reasonable browser support
-import { uglify } from 'rollup-plugin-uglify'
+import buble from '@rollup/plugin-buble' // Transpile/polyfill with reasonable browser support
+import { terser } from 'rollup-plugin-terser'
+import cssnano from 'cssnano'
 
 export default {
   input: 'src/wrapper.js', // Path relative to package.json
   output: {
     name: 'Calculator',
     exports: 'named',
+      sourceMap: false,
+      sourcemap: false,
   },
   plugins: [
     commonjs(),
     vue({
+      sourcemap: false,
+      sourceMap: false,
+      style: {
+        postcssPlugins: [cssnano],
+      },
       css: true, // Dynamically inject css as a <style> tag
       compileTemplate: true, // Explicitly convert template to render function
     }),
-    buble(), // Transpile to ES5,
-    uglify({
-      sourcemap: {
-        filename: 'calculator.min.js',
-        url: 'calculator.min.js.map',
-      },
+    buble({
+      objectAssign: true,
+    }), // Transpile to ES5,
+    terser({
+      sourcemap: false,
+      exclude: /\.esm\./,
     }),
   ],
 }
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   lintOnSave: true,
+  productionSourceMap: false,
   chainWebpack: (config) => {
     config
         .plugin('html')
