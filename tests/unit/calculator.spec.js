6c0a2bc72b8f758c40baf2675fa4fe7ee1b7fba7
anthonykoch <goaway@github.com>
2020-01-07 15:59:19 -0700

    Polish up codes

--- tests/unit/calculator.spec.js
+++ tests/unit/calculator.spec.js
@@ -2,19 +2,13 @@ import { shallowMount } from '@vue/test-utils'
 import Calculator from '@/calculator.vue'
 
 describe('calculator.vue', () => {
-  it('renders without crashing', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(Calculator)
-  })
-
   it('at least does simple math', () => {
-    const msg = 'new message'
     const wrapper = shallowMount(Calculator)
 
     const expressions = ['5', '+', '7', '-', '45', '+', '3', '+', '177', '-'];
     const currentOperand = '147';
 
-    expressions.concat(['147']).forEach((input) => {
+    expressions.concat([currentOperand]).forEach((input) => {
       input.split('').forEach((n) => wrapper.find(`[data-id="${n}"]`).trigger('click'))
     })
     expect(wrapper.find('[data-formula]').text()).toMatch(expressions.join(' '))
