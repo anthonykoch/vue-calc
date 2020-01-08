5cb7742a307f40fd042b4ae4e3e459fe982b2fd8
anthonykoch <goaway@github.com>
2020-01-08 05:11:55 -0700

    Update build

--- rollup.config.js
+++ rollup.config.js
@@ -5,18 +5,29 @@ import { terser } from 'rollup-plugin-terser'
 import cssnano from 'cssnano'
 
 export default {
-  input: 'src/wrapper.js', // Path relative to package.json
-  output: {
-    name: 'Calculator',
-    exports: 'named',
-      sourceMap: false,
-      sourcemap: false,
-  },
+  input: './src/wrapper.js', // Path relative to package.json
+  output: [
+    {
+      file: 'build/calculator.umd.js',
+      format: 'umd',
+      plugins: [terser()],
+      name: 'VueCalculator',
+    },
+    {
+      file: 'build/calculator.js',
+      format: 'cjs',
+      plugins: [terser()],
+    },
+    {
+      file: 'build/calculator.esm.js',
+      format: 'esm',
+    },
+  ],
   plugins: [
     commonjs(),
     vue({
-      sourcemap: false,
-      sourceMap: false,
+      // sourcemap: false,
+      // sourceMap: false,
       style: {
         postcssPlugins: [cssnano],
       },
@@ -26,9 +37,6 @@ export default {
     buble({
       objectAssign: true,
     }), // Transpile to ES5,
-    terser({
-      sourcemap: false,
-      exclude: /\.esm\./,
-    }),
   ],
+  external: ['Vue', 'vue-runtime-helpers'],
 }
